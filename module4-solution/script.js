b38b8ae4eeb3a0600fad9a5bc15a0ede5b2fc85f
yuvrajsinh4509 <124519631+yuvrajsinh4509@users.noreply.github.com>
2023-02-06 13:00:41 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,1 +1,17 @@
+(function () {
 
+    var names = ["Harry", "JJ", "Simon", "Ethan", "Vik", "Tobi", "Josh", "Klaus", "Elijah", "Niko"];
+    
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+      if (firstLetter === 'j') {
+        byeSpeaker(names[i]);
+      }
+      else {
+        helloSpeaker(names[i]);
+      }
+    }
+    
+    })();
+    
